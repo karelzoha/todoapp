ead6e629c9eeb2081a69e30cea079e8fc00e2058
Karel Žoha <karel.zoha@gmail.com>
2017-03-17 11:33:15 +0100

    feat: add server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-const name = 'world'
-console.log(`Hello ${name}!`)
+'use strict'
+const http = require('http') 
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    res.write('Hello world!')
+    res.end()
+})
+
+server.on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+})
+
+server.listen(PORT)
