13ce3ba7e29138f2a4b622356aa337befd26b764
Karel Žoha <karel.zoha@gmail.com>
2017-04-07 12:58:32 +0200

    refactor: add expressjs

--- src/server.js
+++ src/server.js
@@ -1,20 +1,14 @@
 import http from 'http'
 import url from 'url'
+import express, { Router } from 'express'
 
+export default function({ store }) {
+    const app = express()
+    const router = Router()
 
-function processPost(req, res, store) {
-    console.log('jsem tu')
-    let body = ''
-    req.setEncoding('utf8')    
-
-    req.on('data', (data) => {
-        console.log('data: ', data)
-        body = body + data
-    })
-
-    req.on('end', () => {
-        console.log('body: ', body)
-        res.statusCode = 200
+    router.get('/task/:taskId', (req, res) => {
+        const taskId = req.params.taskId
+        const body = JSON.stringify(store.getTask(parseInt(taskId, 10)))
         res.writeHead(200, {
             'Content-Length': Buffer.byteLength(body),
             'Content-Type': 'application/json; charset=utf-8'
@@ -22,43 +16,31 @@ function processPost(req, res, store) {
         res.write(body)
         res.end()
     })
-}
 
-export default function({ store }) {
-    const server = http.createServer((req, res) => {
-        const parsedUrl = url.parse(req.url)
-        const parsedTaskUrl = parsedUrl.pathname.match(/\/v1\.0\.0\/task(\/)?([0-9]*)?/)
-        if (typeof parsedUrl.pathname === 'string' && parsedTaskUrl) {
-            const taskId = parseInt(parsedTaskUrl[2], 10)
-            if(req.method === 'GET' && !isNaN(taskId)) {
-                const body = JSON.stringify(store.getTask(taskId))
-                res.writeHead(200, {
-                    'Content-Length': Buffer.byteLength(body),
-                    'Content-Type': 'application/json; charset=utf-8' 
-                })
-                res.write(body)    
-                res.end()
-            } else if(req.method === 'POST' && isNaN(taskId)) {
-                processPost(req, res, store)
-            } else {
-                res.statusCode = 400
-                res.statusMessage = 'Not implemented'
-                res.end()
-            }
-        } else {
-            res.statusCode = 400
-            res.statusMessage = 'Bad request'
+    router.post('/task', (req, res) => {
+        let body = ''
+        req.setEncoding('utf8')
+
+        req.on('data', (data) => {
+            body = body + data
+        })
+
+        req.on('end', () => {
+            res.statusCode = 200
+            res.writeHead(200, {
+                'Content-Length': Buffer.byteLength(body),
+                'Content-Type': 'application/json; charset=utf-8'
+            })
+            res.write(body)
             res.end()
-        }
+        })
     })
 
-    server.on('clientError', (err, socket) => {
-        socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
-    })
+    app.use('/v1.0.0/', router)
 
     return {
-        run: function(port) {
-            server.listen(port, () => {
+        run: function (port) {
+            app.listen(port, () => {
                 console.log('server listening on port: ', port)
             })
         }
