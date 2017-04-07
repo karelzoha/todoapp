f646092d9ffca3ac7fdfa07ba690a499745bf1ad
Karel Žoha <karel.zoha@gmail.com>
2017-04-07 11:13:27 +0200

    feat: add zakladni implementace zpracovani post requestu

--- src/server.js
+++ src/server.js
@@ -2,14 +2,35 @@ import http from 'http'
 import url from 'url'
 
 
+function processPost(req, res, store) {
+    console.log('jsem tu')
+    let body = ''
+    req.setEncoding('utf8')    
+
+    req.on('data', (data) => {
+        console.log('data: ', data)
+        body = body + data
+    })
+
+    req.on('end', () => {
+        console.log('body: ', body)
+        res.statusCode = 200
+        res.writeHead(200, {
+            'Content-Length': Buffer.byteLength(body),
+            'Content-Type': 'application/json; charset=utf-8'
+        })
+        res.write(body)
+        res.end()
+    })
+}
 
 export default function({ store }) {
     const server = http.createServer((req, res) => {
         const parsedUrl = url.parse(req.url)
-        const parsedTaskUrl = parsedUrl.pathname.match(/\/v1\.0\.0\/task(\/)?([0-9]*)/)
+        const parsedTaskUrl = parsedUrl.pathname.match(/\/v1\.0\.0\/task(\/)?([0-9]*)?/)
         if (typeof parsedUrl.pathname === 'string' && parsedTaskUrl) {
             const taskId = parseInt(parsedTaskUrl[2], 10)
-            if(req.method === 'GET' && taskId !== NaN) {
+            if(req.method === 'GET' && !isNaN(taskId)) {
                 const body = JSON.stringify(store.getTask(taskId))
                 res.writeHead(200, {
                     'Content-Length': Buffer.byteLength(body),
@@ -17,6 +38,8 @@ export default function({ store }) {
                 })
                 res.write(body)    
                 res.end()
+            } else if(req.method === 'POST' && isNaN(taskId)) {
+                processPost(req, res, store)
             } else {
                 res.statusCode = 400
                 res.statusMessage = 'Not implemented'
